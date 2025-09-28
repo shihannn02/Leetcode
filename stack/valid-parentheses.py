ce63f2fc452ed94d543972a40e8ac6e06f884858
Shihan Wu <182936746+shihannn02@users.noreply.github.com>
2025-09-28 23:39:35 +0800

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

---
+++ stack/valid-parentheses.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {')': '(', ']':'[', '}':'{'}
+
+        for i in s:
+            if i not in mapping.values():
+                stack.append(i)
+            else:
+                if not stack or stack[-1]!=mapping[i]:
+                    return False
+        return not stack
